172c41b891ab53328210af7c30b3c080c2ec2da0
Bubbles <harsyax1@gmail.com>
2024-07-10 20:47:39 +0700

    Add text on secondpage

--- lib/pages/secondpage.dart
+++ lib/pages/secondpage.dart
@@ -5,6 +5,12 @@ class Secondpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [Text('Rentalin')],
+        ),
+      ),
+    );
   }
 }
